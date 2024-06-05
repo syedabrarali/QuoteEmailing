a2f3ee998a68c6eae10f9d4848e073a251a942ca
Syed Abrar Ali <abrar.ali75@gmail.com>
2024-06-05 16:09:47 +0530

    added DB logic, to store and check for the hadeeses that have been sent and so that they don't repeat, added env variables for db and min and max for randomNumber

--- quote.js
+++ quote.js
@@ -1,45 +1,93 @@
 const nodemailer = require("nodemailer");
+const { Pool } = require("pg");
+require('dotenv').config();
 let reciepientsArray = ['abrar.ali75@gmail.com',
 'zayedali768@gmail.com', 'simeensheikh108@gmail.com', 'nihal.syed2012@gmail.com', 'ashhamareeb8@gmail.com', 'zakiramariam123@gmail.com', 'frkrocks@gmail.com']
 let reciepients = reciepientsArray.toString();
+let hadees_number;
+let hadees_number_rowCount;
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
+  const transporter = nodemailer.createTransport({
+    host: process.env.NODEMAILER_HOST,
+    port: process.env.NODEMAILER_PORT,
     secure: true, // Use `true` for port 465, `false` for all other ports
     auth: {
-      user: 'abrar.ali75@gmail.com',
-      pass: 'saejwrjyxopcjdpr',
+      user: process.env.NODEMAILER_AUTH_USER,
+      pass: process.env.NODEMAILER_AUTH_PW,
     },
   });
 
-  //using this to generate a random hadees
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+    // using this to generate a random hadees
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-  // used max=4321 as the inb majah has 4341 hadees in them
-  // This below code snippet to store the random numbers doesn't work because the 
-  //randomArray gets initialised everytime we run the code, so need to store that data
-  //in a db and fetch it everytime we want to check if the hadees number would be repeated
-  //or not
+  //connection to local postgres db
+  const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    password: process.env.PG_PW,
+    port: process.env.PG_PORT,
+  });
 
-  let randomArray = []; 
-  let randomNumber = getRandomInt(1, 4341);
-  randomArray.push(JSON.parse(JSON.stringify(randomNumber))); //this deep copies the randomNumber object
-  
+  // used max=4321 as the Sunan Ibn Majah has 4341 hadees in the
+  let randomNumber = getRandomInt(process.env.HADEES_START_NO, process.env.HADEES_END_NO);
 
-  //TODO
-  //1.Add the randomNumber generated on each execution to an array and add logic to before trigerring the hadees api
-  // that if the hadees number has been generated before then we should generate a new number.
-  //2. Figure out a way to get the total number of hadees from the endpoint
+  async function getHadeesDatafromPG() {
+    try {
+      const queryText = 'SELECT hadees_no FROM hadees_sent';
+      const res = await pool.query(queryText);
+      hadees_number = res.rows;
+      hadees_number_rowCount = res.rowCount;
+      console.log(res.rows);
+      console.log(hadees_number);
+      console.log(hadees_number_rowCount);
+    } catch (error) {
+      console.log(error);
+    } 
+  };
+
+  getHadeesDatafromPG();
+
+  async function addHadeesDatatoPG() {
+    try {
+      const queryText = `INSERT INTO hadees_sent (hadees_no) VALUES ('${randomNumber}')`;
+      const res = await pool.query(queryText);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
 async function getQuote() {
     try {
         const quoteResponse = await fetch('https://zenquotes.io/api/random');
         const quoteData = await quoteResponse.json();
+
+
+        // below is used to check if the randomNumber generated has already been used before or not
+        for (let i = 0; i < hadees_number_rowCount; i++) {
+          if (randomNumber === hadees_number[i].hadees_no) {
+            randomNumber = getRandomInt(1, 4341);
+            console.log(`the random number generated matched an already sent hadees no so the newly generated random number is ${randomNumber}`);
+            addHadeesDatatoPG();
+        };
+      }
+
+        //if the generated randomNumber hasn't been used, then I am logging it below and then added it to the db.
+        console.log(`the random number generated ${randomNumber} was unqiue and wasn't found in the already sent hadees numbers and it will used to called the hadees api`);
+        try {
+          const queryText = `INSERT INTO hadees_sent (hadees_no) VALUES ('${randomNumber}')`;
+          const res = await pool.query(queryText);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          await pool.end();
+        }
+
         const hadithResponse = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/editions/eng-ibnmajah/${randomNumber}.min.json`);
         const hadithData = await hadithResponse.json();
         let timestamp = new Date().toLocaleString();
@@ -48,7 +96,7 @@ async function getQuote() {
          console.log(`Zen quote of the day: "${quoteData[0].q}" by "${quoteData[0].a}"`);
         let mailOptions = {
             from: '"ZenTimes" <abrar.ali75@gmail.com>', // sender address
-            to: reciepients,
+            to: 'abrar.ali75@gmail.com',
             subject: "Quotes of the Day🪷", // Subject line
             text: `Quotes of the day: Hadith is from the book - ${hadithData.metadata.name}, 
                    hadith Number - ${hadithData.hadiths[0].hadithnumber} and the hadees is 
